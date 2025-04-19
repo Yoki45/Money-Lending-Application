0c7f2382936169c33691f4726e7523a01aca513b
okiyo <okiyostephen@gmail.com>
2025-04-19 18:30:08 +0300

    feat: update loanController class

--- src/main/java/com/lms/system/loan/controller/LoanController.java
+++ src/main/java/com/lms/system/loan/controller/LoanController.java
@@ -3,7 +3,12 @@ package com.lms.system.loan.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lms.generic.dto.ErrorResponseDTO;
 import com.lms.generic.dto.ResponseDTO;
+import com.lms.generic.localization.ILocalizationService;
+import com.lms.system.loan.dto.LoanLimitDTO;
+import com.lms.system.loan.dto.LoanReportDTO;
 import com.lms.system.loan.dto.LoanRequestDTO;
+import com.lms.system.loan.enums.LoanStatus;
+import com.lms.system.loan.model.Loan;
 import com.lms.system.loan.service.ILoanService;
 import com.lms.utils.Utils;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,10 +23,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/loan", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,6 +34,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoanController {
 
     private final ILoanService loanService;
+
+    private final ILocalizationService localizationService;
 
     @Operation(summary = "Request for loan ")
     @ApiResponses({
@@ -47,6 +51,103 @@ public class LoanController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDTO(Utils.STATUS_200, loanService.requestForLoan(loanRequestDTO)));
+
+    }
+
+
+    @Operation(summary = "apply late fees manually")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status CREATED"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    }
+    )
+    @PutMapping("late-fee")
+    public ResponseEntity<ResponseDTO> applyLateFeeManually() {
+        loanService.applyLateFeesToOvedueLoans();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDTO(Utils.STATUS_200, localizationService.getMessage("message.200.ok", null)));
+
+    }
+
+
+    @Operation(summary = "sent Loan Reminders manually")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status CREATED"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    }
+    )
+    @PutMapping("reminder")
+    public ResponseEntity<ResponseDTO> sendReminderMessagesManually() {
+        loanService.sendDueDateReminders();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDTO(Utils.STATUS_200, localizationService.getMessage("message.200.ok", null)));
+
+    }
+
+
+    @Operation(summary = "sent Loan Reminders manually")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status CREATED"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    }
+    )
+    @PutMapping("update-overdue")
+    public ResponseEntity<ResponseDTO> sweepOverdueLoans() {
+        loanService.sweepOverdueLoans();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDTO(Utils.STATUS_200, localizationService.getMessage("message.200.ok", null)));
+
+    }
+
+    @Operation(summary = "Consolidate open loan due dates")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status CREATED"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    }
+    )
+    @PutMapping("consolidate")
+    public ResponseEntity<ResponseDTO> consolidateOpenLoans(@Valid @RequestBody LoanRequestDTO loanRequestDTO) throws JsonProcessingException {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDTO(Utils.STATUS_200, loanService.consolidateLoanDueDates(loanRequestDTO)));
+
+    }
+
+
+    @Operation(summary = "Fetch loan details REST API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status CREATED"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    }
+    )
+
+
+    @GetMapping()
+    public ResponseEntity<LoanReportDTO> fetchLoanDetails(@RequestParam(required = false) LoanStatus status,
+                                                          @RequestParam(required = false) Long loan, @RequestParam(required = false) String range,
+                                                          @RequestParam(required = false) Long customer, @RequestParam(required = false) Long product, @RequestParam(required = false) Long accountNumber,
+                                                          @RequestParam(required = false) Integer page) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(loanService.fetchLoanDetails(status, loan, range, customer, product, accountNumber, page));
 
     }
 
